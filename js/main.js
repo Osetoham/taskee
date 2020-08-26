47683fee8df86ede442065aec6f351cc2deb8af7
Mercy Okobi <mercyokobi@gmail.com>
2020-08-26 17:10:41 +0100

    loading process

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const dropdownButtons = document.querySelectorAll(".js-dropdown");
 
-dropdownButtons.forEach((dropDownButton) => {
+dropdownButtons.forEach((dropDownButton, index) => {
   let showMenu = false;
 
+  console.log(dropDownButton)
   dropDownButton.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
@@ -16,8 +17,25 @@ dropdownButtons.forEach((dropDownButton) => {
   }
 });
 
+
+//dragular code
 const dragularContainerArray = document.querySelectorAll(
   ".js-dragular-container"
 );
-
 dragula(Array.from(dragularContainerArray));
+
+
+
+
+
+
+//loader code
+const loader = document.querySelector(".loader")
+
+window.addEventListener("load", () => {
+  loader.classList.add("hide")
+  AOS.init({
+    duration: 1000
+})
+});
+
